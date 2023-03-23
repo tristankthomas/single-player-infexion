2a9c57a452c7c1bb8eed1262a9732947b26959bb
tristankthomas <tristankthomas@icloud.com>
2023-03-23 11:46:15 +1100

    Skeleton code added

---
+++ src/program.py
@@ -0,0 +1,30 @@
+# COMP30024 Artificial Intelligence, Semester 1 2023
+# Project Part A: Single Player Infexion
+
+from utils import render_board
+
+
+def search(input: dict[tuple, tuple]) -> list[tuple]:
+    """
+    This is the entry point for your submission. The input is a dictionary
+    of board cell states, where the keys are tuples of (r, q) coordinates, and
+    the values are tuples of (p, k) cell states. The output should be a list of 
+    actions, where each action is a tuple of (r, q, dr, dq) coordinates.
+
+    See the specification document for more details.
+    """
+
+    # The render_board function is useful for debugging -- it will print out a 
+    # board state in a human-readable format. Try changing the ansi argument 
+    # to True to see a colour-coded version (if your terminal supports it).
+    print(render_board(input, ansi=False))
+
+    # Here we're returning "hardcoded" actions for the given test.csv file.
+    # Of course, you'll need to replace this with an actual solution...
+    return [
+        (5, 6, -1, 1),
+        (3, 1, 0, 1),
+        (3, 2, -1, 1),
+        (1, 4, 0, -1),
+        (1, 3, 0, -1)
+    ]
